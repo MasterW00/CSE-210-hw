f028335482649efd360b86eb5d335f1dfadf6b33
Jared Farnbach <jaredfarnbach@gmail.com>
2024-04-27 13:55:49 -0600

    Added case statments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,18 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your Percentile grade?\n>");
+        var grade = Console.Read();
+        char letter;
+        if (grade >= 90) letter = 'A';
+        else if (grade >= 80) letter = 'B';
+        else if (grade >= 70) letter = 'C';
+        else if (grade >= 60) letter ='D';
+        else letter = 'F';
+
+        Console.WriteLine($"Your letter Grade {letter}");
+        
     }
 }
