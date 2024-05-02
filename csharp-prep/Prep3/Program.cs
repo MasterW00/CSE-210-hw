efa167f8dcbfa31baba0642df9b1f2c2ca4e0f95
Jared Farnbach <jaredfarnbach@gmail.com>
2024-05-01 22:41:41 -0600

    Finish prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,27 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write($"Pick a magic number\n>");
+        int target = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Hand the computer to your friend now!\nGuess the number!");
+        int attempt = 0;
+        while(true){
+            Console.Write($">");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess == target) break;
+            if(guess > target){
+                Console.WriteLine($"Your guess is too high...");
+            }        
+            if(guess < target){
+                Console.WriteLine($"Your guess is too low...");
+            }
+            attempt++;
+        }
+        Console.Write($"You're right the number is {target}!\n");
+        Console.WriteLine($"It took you {attempt} tries.");
+        
     }
 }
