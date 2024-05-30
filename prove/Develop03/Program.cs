2cb5d99d0c47be92e51e1495b67f2ace2ae2ba12
masterw00 <jaredfarnbach@gmail.com>
2024-05-30 14:47:27 -0700

    Changed  hide bugs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,13 +3,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Scripture first = new Scripture("BookOfMormon", "2 Nephi", 1, 2, 5);
+        Scripture first = new Scripture("BookOfMormon", "2 Nephi", 100, 33);
         Standard_Works.findVerse(first);
-        while(true){
-            first.printScripture();
-        if (first.hideWords()) Environment.Exit(0);
-            if (Console.ReadLine().Contains('q')) break;
+        bool x = true;
+        first.printScripture();
+        string[] scripture = first.getText().Split(' ');
+        while(x){
+            scripture = hideWords(scripture, 10);
+        x = false;
+        foreach(string s in scripture) {
+            if(!s.Contains('_')) x = true;
         }
+            if (Console.ReadLine().Contains('q')) break;
+            Console.Clear();
+            Console.WriteLine($"{string.Join(' ', scripture)}");
+        }
+    }
+    public static string[] hideWords(string[] scripture_redacted, int number){
+        Random random = new Random();
+        int rand;
+        for(int i = 0; i < number; i++){
+            rand = random.Next(0, scripture_redacted.Length - 1);
+            int x = scripture_redacted.Length;
+            while(scripture_redacted[rand].Contains('_') && x > -1 ){
+                if (rand++ >= scripture_redacted.Length - 1){
+                    rand = 0;
+                }
+                x--;
+            }
+            scripture_redacted[rand] = new string('_', scripture_redacted[rand].Length);
+        }
+        return scripture_redacted;
+        
     }
 }
 
